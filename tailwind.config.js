92a7e30dce57eb39162d5eae0d7a99091ff1fe8e
Emon0860 <emon.webdev60@gmail.com>
2025-01-03 20:10:37 +0600

    paytm home page

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: "Inter, serif",
+      },
+      colors: {
+        "nav-blue": "#00baf2",
+        "nav-black": "#002970",
+        "nav-white": "#e5e7eb",
+        "body-blue": "#e0f5fd",
+        "border-color": "#62d3f6",
+        "label-color": "#7d7d7d",
+        "body-gray": "#808080",
+        "neavy-blue": "#0f4a8a",
+        "custom-white": "#f5f7fa",
+        "span-color": "#75767f",
+        "custom-black": "#333333",
+      },
+    },
+  },
+  plugins: [],
+};
